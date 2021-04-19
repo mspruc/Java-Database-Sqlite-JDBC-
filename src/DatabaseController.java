2a2a3b5203775fd482c9a67aa8efef7b58070971
mspruc <55182629+mspruc@users.noreply.github.com>
2021-04-19 09:25:28 +0200

    Test commit db

---
+++ src/DatabaseController.java
@@ -0,0 +1,11 @@
+import java.sql.SQLException;
+
+public class DatabaseController {
+
+    public void start(){
+        String url="jdbc:sqlite:C:/Users/liner/Documents/SD/SD2021S/FridayApril9thTrainDB.db";
+
+
+    }
+
+}
